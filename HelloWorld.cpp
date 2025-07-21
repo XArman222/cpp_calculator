3548f34ec69952486ffefcbed142cefc337f9d8a
XArman222 <xsoulop@gmail.com>
2025-07-21 23:37:07 +0530

    First Program

---
+++ HelloWorld.cpp
@@ -0,0 +1,18 @@
+// Name - Arman Majhi
+// PRN - 24070123022
+// Batch - E&TC - A1
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "hello!"<<endl;
+    cout << "First Program"<<endl;
+    return 0;
+}
+/*
+hello!
+First Program
+
+=== Code Execution Successful ===
+*/
